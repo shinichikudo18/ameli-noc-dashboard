f6b42523043988fd58e0ba28ef662906dee2d9ee
AMELI Dashboard <ameli-dashboard@local>
2026-04-15 18:15:59 -0400

    Disable cache for dashboard data refresh

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ class MyHandler(SimpleHTTPRequestHandler):
         super().__init__(*args, directory='/tmp', **kwargs)
     
     def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
         self.send_header('Content-Type', 'text/html; charset=utf-8')
         self.send_header('Access-Control-Allow-Origin', '*')
         super().end_headers()
